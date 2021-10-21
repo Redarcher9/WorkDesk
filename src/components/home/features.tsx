de91f4ad906c308a277fad6b3df1de3c02243e4b
Snehit Paunikar <snehit@Snehits-MacBook-Air.local>
2021-10-21 23:54:24 +0530

    added Css to features component

--- src/components/home/features.tsx
+++ src/components/home/features.tsx
@@ -1,42 +1,43 @@
 import {Card,Button, Container, Row,Col} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import "../styles/featuresStyles.css";
 
-function Features():JSX.Element {
+function features():JSX.Element {
 
     return(
         <div className="featuresclass">
-        <Container>
-            <Row className="justify-content-center">
-                <Col md={{ span: 3, offset: 0 }}>
-                    <Card className="featurecard">
-                        <Card.Title > TODO</Card.Title>
-                        <Card.Text >
-                                Create a Todo list to keep a track on your tasks.
-                        </Card.Text>
-                            <Link to="/todos" id="featurebutton">
-                                <Button variant="primary">
-                                Get Started
-                                </Button>
-                            </Link>
-                    </Card>
-                </Col>
-                <Col md={{ span: 3, offset: 3 }}>
-                    <Card className="featurecard">
-                        <Card.Title >  NOTES</Card.Title>
-                        <Card.Text >
-                                Store long notes to keep everything at one place.
-                        </Card.Text>
-                            <Link to="/notes" id="featurebutton">
-                                <Button variant="primary">
-                                Get Started
-                                </Button>
-                            </Link>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
+            <Container>
+                <Row className="justify-content-center">
+                    <Col md={{ span: 3, offset: 0 }}>
+                        <Card className="featurecard">
+                            <Card.Title > TODO</Card.Title>
+                            <Card.Text >
+                                    Create a Todo list to keep a track on your tasks.
+                            </Card.Text>
+                                <Link to="/todos" id="featurebutton">
+                                    <Button variant="primary">
+                                    Get Started
+                                    </Button>
+                                </Link>
+                        </Card>
+                    </Col>
+                    <Col md={{ span: 3, offset: 3 }}>
+                        <Card className="featurecard">
+                            <Card.Title >  NOTES</Card.Title>
+                            <Card.Text >
+                                    Store long notes to keep everything at one place.
+                            </Card.Text>
+                                <Link to="/notes" id="featurebutton">
+                                    <Button variant="primary">
+                                    Get Started
+                                    </Button>
+                                </Link>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
 
-export default Features;
+export default features;
